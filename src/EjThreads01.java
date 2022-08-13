4d119d56de29bc72c38687b53180cd228a8ad9ee
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-08-13 12:53:16 -0500

    Primer commit infracomp

---
+++ src/EjThreads01.java
@@ -0,0 +1,13 @@
+
+public class EjThreads01 extends Thread {
+	public void run()
+	{
+		System.out.println("Extendiendo la clase Thread.");
+	}
+	
+	
+	public static void main(String[] args) {
+		EjThreads01 threads01 = new EjThreads01();
+		threads01.start();
+	}
+}
---
+++ src/EjThreads02.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author jj
+ *Creación de threads como implementación de la interfaz Runnable
+ */
+public class EjThreads02 implements Runnable {
+
+	@Override
+	public void run() {
+		System.out.println("Implementando la interfaz Runnable.");
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Thread t = new Thread(new EjThreads02());
+		
+		t.start();
+	}
+
+}
