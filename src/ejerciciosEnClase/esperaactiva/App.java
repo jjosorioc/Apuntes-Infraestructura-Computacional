273b23ebd94dfc77644a51f8ac76ee31db9160bb
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-08-24 11:06:24 -0500

    Video del guati

---
+++ src/ejerciciosEnClase/esperaactiva/App.java
@@ -0,0 +1,48 @@
+package ejerciciosEnClase.esperaactiva;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        final Processor processor = new Processor();
+
+        /*
+         * Creación thread 1
+         */
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    processor.produce();
+                } catch (InterruptedException e) {
+
+                }
+            }
+        });
+
+
+        /*
+         * Creación thread 2
+         */
+        Thread t2 = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    processor.consume();
+                } catch (InterruptedException e) {
+
+                }
+            }
+        });
+
+
+        // Start
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+
+    }
+}
