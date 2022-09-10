38d60b6f6b45b62e7bedf7d7218955a0c6b4d966
Juan José Osorio <j.osorioc@uniandes.edu.co>
2022-09-10 12:12:57 -0500

    Lab5 Servidor Monothread

---
+++ src/laboratorios/lab5/monothread/Servidor.java
@@ -0,0 +1,53 @@
+package laboratorios.lab5.monothread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+    public static final int PUERTO = 3400;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket ss = null;
+        boolean continuar = true;
+
+        System.out.println("Main Server ...");
+
+        try {
+            ss = new ServerSocket(PUERTO);
+        } catch (Exception e) {
+            System.out.println("Error al crear el socket");
+            System.exit(-1);
+        }
+
+
+        while (continuar) {
+            // Crea el socket en el lado servidor
+            // Queda bloqueado esperando la conexión del cliente
+            Socket socket = ss.accept();
+
+            try {
+                // Se conectan los flujos, tanto de salida como de entrada
+                PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader lector =
+                        new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+
+                // Se ejectura el protocolo en el lado servidor
+                ProtocoloServidor.procesar(lector, escritor);
+
+                // Se cierran los flujos y el socket
+                escritor.close();
+                lector.close();
+                socket.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
